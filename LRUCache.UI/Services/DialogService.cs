6572a95660e635c7b400650c59be0fca254f614a
Ivan Kondrashov <210524135+ivan-kondrashov@users.noreply.github.com>
2026-02-24 22:56:05 +0300

    feat: add show messages functions

--- LRUCache.UI/Services/DialogService.cs
+++ LRUCache.UI/Services/DialogService.cs
@@ -1,3 +1,4 @@
+using System.Windows;
 using Microsoft.Win32;
 
 namespace LRUCache.UI.Services;
@@ -12,5 +13,20 @@ public class DialogService : IDialogService
         };
 
         return dialog.ShowDialog() == true ? dialog.FileName : null;
+    }
+
+    public void ShowMessage(string message)
+    {
+        MessageBox.Show(message, "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+
+    public void ShowErrorMessage(string errMessage)
+    {
+        MessageBox.Show(errMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+    }
+
+    public void ShowWarningMessage(string warnMessage)
+    {
+        MessageBox.Show(warnMessage, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
     }
 }
--- LRUCache.UI/Services/IDialogService.cs
+++ LRUCache.UI/Services/IDialogService.cs
@@ -3,4 +3,7 @@ namespace LRUCache.UI.Services;
 public interface IDialogService
 {
     string? OpenFileDialog(string filter);
+    void ShowErrorMessage(string errMessage);
+    void ShowMessage(string message);
+    void ShowWarningMessage(string warnMessage);
 }
